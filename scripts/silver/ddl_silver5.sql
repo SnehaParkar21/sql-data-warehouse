0262138296f13950d6d90b650839041eac80e4e9
SnehaParkar21 <snehaparkar00@gmail.com>
2025-03-28 22:28:16 +0530

    Update ddl_silver5.sql

--- scripts/silver/ddl_silver5.sql
+++ scripts/silver/ddl_silver5.sql
@@ -1,1 +1,36 @@
+SELECT
+cid,
+cntry
+FROM bronze.erp_loc_a101;
+
+SELECT cst_key FROM silver.crm_cust_info;
+
+SELECT DISTINCT
+cntry
+FROM bronze.erp_loc_a101
+ORDER BY cntry
+
+SELECT DISTINCT
+cntry
+FROM silver.erp_loc_a101
+ORDER BY cntry
+
+SELECT
+cid,
+cntry
+FROM silver.erp_loc_a101;
+
+INSERT INTO silver.erp_loc_a101 (
+			cid,
+			cntry
+		)
+		SELECT
+			REPLACE(cid, '-', '') AS cid, 
+			CASE
+				WHEN TRIM(cntry) = 'DE' THEN 'Germany'
+				WHEN TRIM(cntry) IN ('US', 'USA') THEN 'United States'
+				WHEN TRIM(cntry) = '' OR cntry IS NULL THEN 'n/a'
+				ELSE TRIM(cntry)
+			END AS cntry -- Normalize and Handle missing or blank country codes
+		FROM bronze.erp_loc_a101;
 
